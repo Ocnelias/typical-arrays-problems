34f81daf1fb96803402370f2f1f26a78e12904ce
ocnelias <ocnelias@gmail.com>
2021-03-10 21:50:53 +0200

    feat: implement functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array !== undefined && array.length > 0) {
+        let min = 999999999;
+        for (let i = 0; i <= array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    } else {
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array !== undefined && array.length > 0) {
+        let max = 0;
+        for (let i = 0; i <= array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    } else {
+        return 0;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array !== undefined && array.length > 0) {
+        let sum = 0;
+        let avg = 0;
+        for (let i = 0; i <= array.length - 1; i++) {
+            sum += array[i];
+        }
+        avg = sum / array.length;
+
+        return avg;
+    } else {
+        return 0;
+    }
 }
